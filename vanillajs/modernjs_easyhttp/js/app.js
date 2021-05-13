dc384ca6b3a82b6d632fb1b12f624ae4a1c5b190
Tomasz Dopierala <tomasz@dopierala.net.pl>
2021-05-13 14:32:16 +0200

    Add EasyHTTP project

---
+++ vanillajs/modernjs_easyhttp/js/app.js
@@ -0,0 +1,39 @@
+const data = {title: 'Custom Post', body: 'This is a custom post'}
+
+// * Calls for version 1.0 (ES5)
+/*
+const http = new easyHTTP();
+
+http.get('http://jsonplaceholder.typicode.com/posts', callbackFunc);
+
+http.post('http://jsonplaceholder.typicode.com/posts', data, callbackFunc);
+
+http.put('http://jsonplaceholder.typicode.com/posts/1', data, callbackFunc);
+
+http.delete('http://jsonplaceholder.typicode.com/posts/1', callbackFunc);
+
+function callbackFunc(error, response) {
+    if(error) console.log(error);
+    else console.log(response);
+}
+ */
+
+// * Calls for version 2.0 (ES6) and version 3.0 (ES7)
+
+const http = new EasyHTTP;
+
+http.get('http://jsonplaceholder.typicode.com/posts')
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+http.post('http://jsonplaceholder.typicode.com/posts', data)
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+http.put('http://jsonplaceholder.typicode.com/posts/1', data)
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+http.delete('http://jsonplaceholder.typicode.com/posts/1', data)
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
---
+++ vanillajs/modernjs_easyhttp/js/easyhttp1.js
@@ -0,0 +1,87 @@
+/**
+ * EeasyHTTP Library
+ * Library for making HTTP requests
+ * 
+ * @version 1.0
+ * @author tdopierala
+ * @license MIT
+ * 
+ */
+
+function easyHTTP() {
+    this.http = null; // new XMLHttpRequest();
+}
+
+// make http get request
+easyHTTP.prototype.get = function(url, callback) {
+    // console.log('calling get method');
+
+    this.http = new XMLHttpRequest();
+    
+    this.http.open('GET', url, true);
+
+    let self = this;
+    this.http.onload = function() {
+        if(self.http.status === 200) {
+            callback(null, self.http.responseText);
+        } else {
+            callback('Error: ' + self.http.status);
+        }
+    }
+
+    this.http.send();
+}
+
+// make http post request
+easyHTTP.prototype.post = function(url, data, callback) {
+    // console.log('calling post method');
+
+    this.http = new XMLHttpRequest();
+    
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function() {
+        callback(null, self.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+}
+
+// make http put request
+easyHTTP.prototype.put = function(url, data, callback) {
+    // console.log('calling put method');
+
+    this.http = new XMLHttpRequest();
+    
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function() {
+        callback(null, self.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+}
+
+// make http delete request
+easyHTTP.prototype.delete = function(url, callback) {
+    // console.log('calling delete method');
+
+    this.http = new XMLHttpRequest();
+    
+    this.http.open('DELETE', url, true);
+
+    let self = this;
+    this.http.onload = function() {
+        if(self.http.status === 200) {
+            callback(null, self.http.responseText);
+        } else {
+            callback('Error: ' + self.http.status);
+        }
+    }
+
+    this.http.send();
+}

a9737a02d7722f1286acccd53e6b45566788e55b
Tomasz Dopierala <tomasz@dopierala.net.pl>
2022-03-17 11:35:53 +0100

    Vue animations update

---
+++ vuejs/animations/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="mt-3">
+        <div class="card mb-3">
+            <div class="card-body">
+                <div class="block" :class="{ animate: animatedBlock }"></div>
+                <button @click="animateBlock" class="btn btn-info">Animate</button>
+            </div>
+        </div>
+
+        <base-modal @close="hideDialog" :open="dialogIsVisible">
+            <p>This is a test dialog!</p>
+            <button class="btn btn-sm btn-primary" @click="hideDialog">Close it!</button>
+        </base-modal>
+
+        <div class="card mb-3">
+            <div class="card-body">
+                <button @click="showDialog" class="btn btn-info">Show Dialog</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    components: {},
+    data() {
+        return {
+            dialogIsVisible: false,
+            animatedBlock: false,
+        };
+    },
+    methods: {
+        animateBlock() {
+            this.animatedBlock = true;
+        },
+        showDialog() {
+            this.dialogIsVisible = true;
+        },
+        hideDialog() {
+            this.dialogIsVisible = false;
+        },
+    },
+};
+</script>
+
+<style>
+.block {
+    width: 8rem;
+    height: 8rem;
+    background-color: turquoise;
+    margin-bottom: 2rem;
+}
+.animate {
+    transform: translateX(+50px);
+}
+</style>
---
+++ vuejs/animations/src/components/BaseModal.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <div class="backdrop" @click="$emit('close')" v-if="open"></div>
+        <transition name="modal">
+            <div class="card base-modal" v-if="open">
+                <div class="card-body">
+                    <dialog open v-if="open">
+                        <slot></slot>
+                    </dialog>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['open'],
+    emits: ['close'],
+};
+</script>
+
+<style scoped>
+.base-modal {
+    position: fixed;
+    top: 10vh;
+    width: 70%;
+    left: 15%;
+    margin: 0;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    z-index: 100;
+    color: white;
+}
+
+.base-modal * {
+    color: white;
+}
+
+dialog {
+    margin: 0;
+    padding: 0;
+    background-color: none;
+    border: none;
+    position: relative;
+}
+
+.backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    z-index: 10;
+    background-color: rgba(0, 0, 0, 0.75);
+}
+
+/* .modal-enter-from {} */
+
+.modal-enter-active {
+    animation: modal 0.3s ease-out;
+}
+
+/* .modal-enter-to {} */
+
+.modal-leave-active {
+    animation: modal 0.3s ease-in reverse;
+}
+
+@keyframes modal {
+    from {
+        opacity: 0;
+        transform: translateY(-50px) scale(0.9);
+    }
+    to {
+        opacity: 1;
+        transform: translateY(0) scale(1);
+    }
+}
+</style>

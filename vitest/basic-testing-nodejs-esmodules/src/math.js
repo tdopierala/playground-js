fceefcf84c13bdef4d0b3729a30a8dd6eb850dc0
Tomasz Dopierala <tomasz@dopierala.net.pl>
2022-05-24 09:52:04 +0200

    Vitest - nodejs - refactoring and adds basic tests

--- vitest/basic-testing-nodejs-esmodules/src/math.js
+++ vitest/basic-testing-nodejs-esmodules/src/math.js
@@ -1,8 +1,25 @@
-export function add(numbers) {
-  let sum = 0;
+import { cleanNumbers } from './util/numbers.js'
 
-  for (const number of numbers) {
-    sum += number;
-  }
-  return sum;
+export function add(numbers) {
+	let sum = 0;
+
+	for (const number of numbers) {
+		sum += +number;
+	}
+	return sum;
+}
+
+export function calculateResult(numberInputs) {
+	let result = '';
+
+	try {
+		const numbers = cleanNumbers(numberInputs);
+
+		result = add(numbers).toString();
+
+	} catch (error) {
+		result = error.message;
+	}
+
+	return result;
 }
---
+++ vitest/basic-testing-nodejs-esmodules/src/util/numbers.test.js
@@ -0,0 +1,74 @@
+import { test, expect, describe } from 'vitest';
+import { cleanNumbers, transformToNumber } from './numbers.js';
+
+describe('transformToNumber() tests', () => {
+
+	test('if it can transform a string to a number', () => {
+		const input = '2';
+
+		const result = transformToNumber(input);
+
+		// const expectedResult = +input;
+		// expect(result).toBe(expectedResult);
+		expect(result).toBeTypeOf('number');
+		expect(result).toBe(+input);
+	});
+
+	test('if it return NaN for non-transformable values', () => {
+		const input1 = 'invalid';
+		const input2 = {};
+
+		const result1 = transformToNumber(input1);
+		const result2 = transformToNumber(input2);
+
+		expect(result1).toBeNaN;
+		expect(result2).toBeNaN;
+	});
+
+});
+
+describe('cleanNumbers() tests', () => {
+
+	test('if it return an error when non iterable type od data is provided', () => {
+		const input1 = {};
+		const input2 = null;
+		const input3 = undefined;
+
+		const result1 = () => cleanNumbers(input1);
+		const result2 = () => cleanNumbers(input2);
+		const result3 = () => cleanNumbers(input3);
+
+		expect(result1).toThrow(/not iterable/);
+		expect(result2).toThrow(/not iterable/);
+		expect(result3).toThrow(/not iterable/);
+	});
+
+	test('if it return an array (object)', () => {
+		const input = ['1', '2'];
+
+		const result = cleanNumbers(input);
+
+		expect(result).toBeTypeOf('object');
+		expect(result.length).toBe(input.length)
+	});
+
+	test('if it returns an array of numbers if an array of string numbers is provided', () => {
+		const input = ['1', '2'];
+
+		const result = cleanNumbers(input);
+
+		expect(result[0]).toBeTypeOf('number');
+		expect(result[1]).toBeTypeOf('number');
+		expect(result[2]).toBeUndefined();
+	});
+
+
+	test('if it throw an error if an array with al least on empty string is provided', () => {
+		const input = ['', 2];
+
+		const result = () => cleanNumbers(input);
+
+		expect(result).toThrow();
+	});
+
+});
